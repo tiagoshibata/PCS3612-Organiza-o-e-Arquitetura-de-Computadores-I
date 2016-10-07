5458ff182c748508a4b4c765b6d02c14483ccd94
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-10-07 08:17:16 -0300

    Changed quick_sort signature, fix recursion

--- Projeto-1/quick_sort.asm
+++ Projeto-1/quick_sort.asm
@@ -1,18 +1,17 @@
 la $a0, v
-add $a1, $zero, $zero
-addi $a2, $zero, 12
+la $a1, v_end + -4
 jal quicksort
 break
 # halt: j halt
 
-quicksort: # quicksort(int v[], int start, int end)
-	slt $t0, $a1, $a2
+quicksort: # quicksort(int *start, int *end)
+	sltu $t0, $a0, $a1
 	# return if start >= end
 	bne $t0, $zero, valid_range
 	jr $ra
 	valid_range:
-	add $t1, $a0, $a1  # $t1 = pointer to smaller numbers
-	add $t2, $a0, $a2  # $t2 = pointer to larger numbers
+	add $t1, $a0, $zero  # $t1 = pointer to smaller numbers
+	add $t2, $a1, $zero  # $t2 = pointer to larger numbers
 	addi $t3, $t1, 4  # $t3 = &v[i + 1]
 	lw $t0, 0($t1)  # $t0 = pivot
 
@@ -51,25 +50,27 @@ quicksort: # quicksort(int v[], int start, int end)
 
 		# exit if high < i
 		three_way_partition_end:
-		sltu $t5, $t2, $3
+		sltu $t5, $t2, $t3
 	beq $t5, $zero, three_way_partition
 
 	# save return address, high and end
 	addi $sp, $sp, -12
 	sw $ra, -8($sp)
 	sw $t2, -4($sp)
-	sw $a2, 0($sp)
+	sw $a1, 0($sp)
+
 	# recurse
-	addi $a2, $t1, -4
+	addi $a1, $t1, -4
 	jal quicksort
 
-	lw $a1, -4($sp)
-	addi $a1, $a1, 4
-	lw $a2, 0($sp)
+	lw $a0, -4($sp)
+	addi $a0, $a0, 4
+	lw $a1, 0($sp)
 	jal quicksort
 
 	lw $ra, -8($sp)
 	addi $sp, $sp, 12
 	jr $ra
 .data
-v: .word 5, 4, 2, 3
+v: .word 5, 4, 2, 3, 9, 1, 12, 1, 1, 1, 3, 4, 2, 3, 6, 4, 2, 8, 4, 14, 14, 5
+v_end:
